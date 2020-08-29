da97839a246877672498d4304873a9f6a0bef4dd
Patraic-Kelly <63427924+Patraic-Kelly@users.noreply.github.com>
2020-08-28 22:36:58 -0600

    Add files via upload

---
+++ afs-splash-page/js/script.js
@@ -0,0 +1,15 @@
+/*********************************************************************
+ ***
+ *Original Author: Patraic Kelly                                       *
+ *Date Created:8/28/2020                                               *
+ *Version:                                                             *
+ *Date Last Modified:8/28/2020                                         *
+ *Modified by:                                                         *
+ *Modification log:                                                    *
+ ***
+ ******************************************************************** */
+
+function chestButton() {
+    alert("Coming soon!");
+  }
+  
