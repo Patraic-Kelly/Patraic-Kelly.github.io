49cc3710e3a2438e5ed20f35fe94d6b899120681
Patraic Kelly <patraickelly@my.cwi.edu>
2020-10-15 23:33:34 -0600

    upload site

---
+++ calendar_app/src/app/App.vue
@@ -0,0 +1,45 @@
+<!-----------------------------------------------------------
+--
+#Date Last Modified: 10/15/20                               #
+#Modified by: Patraic Kelly                                          #
+#Modification log: Built as per fullstack vue book in VSCode #
+ --
+------------------------------------------------------------>
+
+<template>
+  <div id="app">
+    <CalendarWeek />
+    <CalendarEntry />
+  </div>
+</template>
+
+<script>
+import CalendarWeek from './components/CalendarWeek.vue';
+import CalendarEntry from './components/CalendarEntry.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CalendarWeek,
+    CalendarEntry
+  }
+}
+</script>
+
+<style lang="scss">
+html, body {
+  height: 100%;
+}
+</style>
+<style lang="scss" scoped>
+#app {
+  height: inherit;
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+}
+</style>
---
+++ calendar_app/src/app/components/CalendarWeek.vue
@@ -0,0 +1,46 @@
+<!----------------------------------------------------
+--
+#Original Author: Patraic Kelly                #
+#Date Created: 10/15/20                                   #
+#Version:                                                    #
+#Date Last Modified: 10/15/20                               #
+#Modified by:   PK                                        #
+#Modification log: Built as per fullstack vue book in VSCode #
+------------------------------------------------------>
+
+<template>
+  <div id="calendar-week" class="container">
+    <div class="columns is-mobile">
+        <CalendarDay v-for="day in sharedState.seedData"
+          :key="day.id"
+          :day="day" 
+          />
+      </div>
+    </div>
+</template>
+
+<script>
+import { store } from "../store.js";
+import CalendarDay from "./CalendarDay.vue";
+
+export default {
+  name: "CalendarWeek",
+  data () {
+    return {
+      sharedState: store.state
+    };
+  },
+  components: {
+    CalendarDay
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+#calendar-week {
+  margin-bottom: 50px;
+  .column {
+    padding: 0 0 0 0;
+  }
+}
+</style>
